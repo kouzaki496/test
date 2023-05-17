c83f36dcd717eec8a071c14ffa9858a8abc94785
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2023-05-18 01:57:30 +0900

    コメント/メモ

---
+++ TestMVCApp/Controllers/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TestMVCApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public List<string> list;
+
+        public HelloController() 
+        {
+            list = new List<string>();
+            list.Add("Japan");
+            list.Add("USA");
+            list.Add("UK");
+
+        }
+
+        [Route("Hello/{id?}/{name?}")]
+        public IActionResult Index(int id, string name)
+        {
+            ViewData["Hoge"] = "hello!!";
+            ViewData["message"] = "id = " + id + ", name = " + name;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form()
+        {
+            string[] res = (string[])Request.Form["list"];
+            string msg = "※";
+            foreach(var item in res)
+            {
+                msg += "「" + item + "」";
+            }
+
+            ViewData["message"] = msg + " selected.";
+            ViewData["list"] = Request.Form["list"];
+            ViewData["listdata"] = list;
+            return View("Index");
+        }
+    }
+
+}
